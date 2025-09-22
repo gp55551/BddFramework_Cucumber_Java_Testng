e4c27aed49146db74838b8c600b044ec2154f73c
gp55551 <patnaikgaurav61@gmail.com>
2025-09-22 11:24:29 +0530

    login

--- src/main/java/utils/DriverManager.java
+++ src/main/java/utils/DriverManager.java
@@ -2,14 +2,33 @@ package utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverManager {
     private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
+    public static WebDriver getNewDriverInstance(String browserName) {
+        switch (browserName.toLowerCase()) {
+            case "chrome-headless":
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--headless");
+                chromeOptions.addArguments("start-maximized");
+                return new ChromeDriver(chromeOptions);
+            case "firefox":
+                return new FirefoxDriver();
+            case "edge":
+                return new EdgeDriver();
+            default:
+                return new ChromeDriver();
+        }
+    }
+
     public static void initDriver() {
         if (driver.get() == null) {
             // NOTE: Add WebDriver binaries path or use WebDriverManager
-            driver.set(new ChromeDriver());
+            driver.set(getNewDriverInstance(ConfigReader.getProperty("browserName")));
             driver.get().manage().window().maximize();
         }
     }
